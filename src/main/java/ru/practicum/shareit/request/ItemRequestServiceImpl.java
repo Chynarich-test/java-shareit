bb7ef1c9dcd2882631556113118b2fee7fc60c3d
Chynarich-test <chynarich228@gmail.com>
2025-08-25 14:41:49 +0300

    Выполнить реформат кода согласно чекстайлу

--- src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestServiceImpl.java
@@ -3,7 +3,7 @@ package ru.practicum.shareit.request;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ItemRequestServiceImpl implements ItemRequestService{
+public class ItemRequestServiceImpl implements ItemRequestService {
 
     @Override
     public ItemRequest getItemRequest(long id) {
